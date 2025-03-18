d0c5660e76ded130651dec508214909635587e5a
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2025-03-18 17:42:11 +0100

    notebook with coco

---
+++ utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def power_law(x: float, a: float, b: float, c: float) -> float:
+  return a * (x ** b) + c
+
+def stretched_exponential(x: float, a: float, b: float) -> float:
+  return np.exp(-a * (x ** b))
