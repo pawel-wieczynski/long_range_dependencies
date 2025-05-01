e4236462d349ebaf7b7617218b46877894865f7f
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2025-05-01 06:29:07 +0200

    Calculate error metric at runtime of the experiments

--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ def power_law(x: float, a: float, b: float) -> float:
 
 def stretched_exponential(x: float, a: float, b: float, c: float) -> float:
   return np.exp(-a * (x ** b) + c)
+
+def calculate_wssr(y_obs: np.array, y_pred: np.array) -> float:
+    return np.sum((y_obs - y_pred)**2 / y_obs**2)
