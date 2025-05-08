0f34b1c13651714b9d5fe2193e743c35171e8e42
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2025-05-08 07:52:49 +0200

    arsr instead of srsr

--- utils.py
+++ utils.py
@@ -6,5 +6,5 @@ def power_law(x: float, a: float, b: float) -> float:
 def stretched_exponential(x: float, a: float, b: float, c: float) -> float:
   return np.exp(-a * (x ** b) + c)
 
-def calculate_srsr(y_obs: np.array, y_pred: np.array) -> float:
-    return np.sum((y_obs - y_pred)**2 / y_obs**2)
+def calculate_arsr(y_obs: np.array, y_pred: np.array) -> float:
+    return np.mean((y_obs - y_pred)**2 / y_obs**2)
